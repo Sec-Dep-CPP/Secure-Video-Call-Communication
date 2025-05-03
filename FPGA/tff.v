217c5aae31042bab101d5d7d11ed15f3868c614b
sebastianbendezu <zbastian1920@gmail.com>
2025-05-03 16:38:08 -0700

    Create tff.v

---
+++ FPGA/tff.v
@@ -0,0 +1,20 @@
+module tff(
+    input  clk,
+    input  reset,
+    input  t,
+    output reg q
+);
+
+    always @(posedge clk, posedge reset)
+    begin
+        if(reset)
+        q <= 0;
+        else if (t)
+        q <= ~q;
+        else
+        q <= q;
+    end
+
+
+
+endmodule
